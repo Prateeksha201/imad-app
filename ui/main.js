e7b3db9083f7176d1d5f7a0dfe196137ee26349a
samhithacs <30855169+samhithacs@users.noreply.github.com>
2017-08-13 09:53:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,7 @@ var element=document.getElementById('xyz');
 element.innerHTML='hi!im samhitha. this is my first webapp';
 //to move img
 var img=documnet.getElementById('madi');
+var marginLeft=0;
 img.onclick=function() {
     img.style.marginLeft='100px';
 };
